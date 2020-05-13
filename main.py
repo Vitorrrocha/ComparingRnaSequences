09cdad8dd6ce9e0d54df5a981179e04e58dfbb04
Vitor Augusto Batista Rocha <44412471+Vitorrrocha@users.noreply.github.com>
2020-05-13 05:44:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,31 +7,32 @@ bacteria = open("16s_bacteria.fasta").read()
 seq_human = {}
 seq_bacteria = {}
 
-while m == 0:
-    if aux == 0:
+while m == 0:   #Necessario para fazer a leitura dos dois arquivos em uma execução e popular assim os dicionarios.
+    if aux == 0:    #Foi utilizado para que seja possivel a leitura da bacteria e do humano em momentos diferentes.
         entrada = human
     else:
         entrada = bacteria
-        m = 1
+        m = 1   #mudança feita para sair do loop e poder executar os codigos de plotagem do grafico.
 
-    cont = {}
+    cont = {}  #Dicionario que ira receber as bases nitrogenadas.
 
     for i in ['A' ,'T' ,'C' ,'G']:
         for j in ['A' ,'T' ,'C' ,'G']:
-            cont[i + j] = 0
+            cont[i + j] = 0            #Populando o dicionario com cada combinação de bases nitrogenas com valor zero.
 
-    entrada = entrada.replace('\n','')
+    entrada = entrada.replace('\n','')      #substituição necessaria para que o arquivo continue sendo lido.
 
-    for k in range(len(entrada) - 1):
-        cont[entrada[k] + entrada[k + 1]] += 1
-        if aux == 0:
-            seq_human = copy.deepcopy(cont)
+    for k in range(len(entrada) - 1):       #O arquivo será lido completamente em pares, menos 1 pois seria o fim do arquivo não contendo mais pares para sererm lidos.
+        cont[entrada[k] + entrada[k + 1]] += 1  #Toda vez que um caracter (base nitrogenada) mais o proximo for lido ex: "AC", será somado mais um a aquele par no dicionario.
+        
+        if aux == 0: 
+            seq_human = copy.deepcopy(cont)  #variavel que recebe a copia do dicionario atual.
         else:
-            seq_bacteria = copy.deepcopy(cont)
+            seq_bacteria = copy.deepcopy(cont)  #variavel que recebe a copia do dicionario atual.
 
     aux += 1
 
-
+#   Montagem do grafico em barras utilizando dois dicionarios
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 5))
 
 ax1.bar(seq_human.keys(), seq_human.values(), width=0.4, align='edge', label='Human: 18S rRNA gene')
